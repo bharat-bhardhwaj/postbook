0963a530065b3aba492c21d93dbbba1c93581f9c
bharat bhardhwaj <bharatbhardhwaj571@gmail.com>
2020-03-22 16:19:53 +0530

    my server is running

---
+++ index.js
@@ -0,0 +1,23 @@
+const express=require('express');
+
+const app=express();
+
+const port=7000;
+
+app.use('/',require('./routes'))
+
+
+// app.get('/',(req,res)=>{
+//     res.send('my site is working');
+
+
+// })
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`error is running in the server:${err}`);
+
+    }
+
+    console.log(`server is running on the port no ${port}`);
+});
---
+++ routes/index.js
@@ -0,0 +1,3 @@
+const express= require('express');
+
+cont router = express
